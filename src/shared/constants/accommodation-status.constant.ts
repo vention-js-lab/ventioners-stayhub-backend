1540bfd88b0670ba70487b50f3b97a2e122f65e3
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-11 22:09:26 +0500

    feat: accommodation status enum created

---
+++ src/shared/constants/accommodation-status.constant.ts
@@ -0,0 +1,10 @@
+export enum AccommodationStatus {
+  VACANT = 'vacant',
+  OCCUPIED = 'occupied',
+  RESERVED = 'reserved',
+  PENDING_CHECKOUT = 'pending_checkout',
+  UNDER_MAINTENANCE = 'under_maintenance',
+  PENDING_APPROVAL = 'pending_approval',
+  BLOCKED = 'blocked',
+  ARCHIVED = 'archived',
+}
