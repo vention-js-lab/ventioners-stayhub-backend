11043e4d64faf2fe0a55000535533bdd3b096149
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-12 15:52:22 +0500

    feat: accommodation category enum created

---
+++ src/shared/constants/accommodation-category.constant.ts
@@ -0,0 +1,10 @@
+export enum AccommodationCategory {
+  HOTEL = 'hotel',
+  APARTMENT = 'apartment',
+  VILLA = 'villa',
+  RESORT = 'resort',
+  HOSTEL = 'hostel',
+  GUEST_HOUSE = 'guest_house',
+  COTTAGE = 'cottage',
+  CAMPING = 'camping',
+}
