0cb3d415aae0a5247782d18aa76054a5c1daca0a
tiescl <anvar.madvaliev@icloud.com>
2024-11-12 15:18:18 +0500

    chore: add usage example

--- src/shared/guards/auth-token.guard.ts
+++ src/shared/guards/auth-token.guard.ts
@@ -1,4 +1,12 @@
 import { AuthGuard as PAuthGuard } from '@nestjs/passport';
 import { AuthConfig } from '../configs';
 
+/**
+ * Decorate the endpoint inside the
+ * controller to protect the route
+ *
+ * @example
+ * @UseGuards(AuthTokenGuard)
+ *
+ **/
 export class AuthTokenGuard extends PAuthGuard(AuthConfig.AuthTokenKey) {}
