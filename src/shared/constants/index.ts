f9f19855f22e068a11cccf9e7325e3a009fba593
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-12 16:01:13 +0500

    feat: accommodation category added in index for better importing

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,3 +1,4 @@
 export * from './user-role.constant';
 export * from './accommodation-status.constant';
 export * from './amenity-category.constant';
+export * from './accommodation-category.constant';
