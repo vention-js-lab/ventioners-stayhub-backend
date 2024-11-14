e535c2ee3fb70f9190e92979b68b58352cddb803
tiescl <anvar.madvaliev@icloud.com>
2024-11-15 02:28:55 +0500

    fix: remove JWT config as different tokens require different configs

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,22 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { UsersModule } from '../users/users.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        secret: configService.get<string>('AUTH_ACCESS_TOKEN_SECRET'),
-        signOptions: {
-          expiresIn: configService.get<string>('AUTH_ACCESS_TOKEN_EXPIRES_IN'),
-        },
-      }),
-    }),
-    UsersModule,
-  ],
+  imports: [JwtModule, UsersModule],
   providers: [AuthService],
   controllers: [AuthController],
 })
