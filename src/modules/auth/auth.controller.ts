774ca7e83a12e6d807975c017bb07e41e14e9a9c
tiescl <anvar.madvaliev@icloud.com>
2024-11-15 02:36:19 +0500

    feat: add refresh token logic to auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,27 +1,87 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { RegisterDto } from './dto/request/register.dto';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/request/login.dto';
+import { Request, Response } from 'express';
+import { ConfigService } from '@nestjs/config';
+import { REFRESH_TOKEN_COOKIE_AGE } from 'src/shared/constants';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('register')
-  async register(@Body() registerDto: RegisterDto) {
-    const accessToken = await this.authService.register(registerDto);
+  async register(
+    @Body() registerDto: RegisterDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const tokens = await this.authService.register(registerDto);
+
+    res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      secure: this.configService.get('NODE_ENV') === 'production',
+      maxAge: REFRESH_TOKEN_COOKIE_AGE,
+    });
 
     return {
-      data: accessToken,
+      accessToken: tokens.accessToken,
     };
   }
 
   @Post('login')
-  async login(@Body() loginDto: LoginDto) {
-    const accessToken = await this.authService.login(loginDto);
+  async login(
+    @Body() loginDto: LoginDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const tokens = await this.authService.login(loginDto);
+
+    res.cookie('refreshToken', tokens.refreshToken, {
+      httpOnly: true,
+      secure: this.configService.get('NODE_ENV') === 'production',
+      maxAge: REFRESH_TOKEN_COOKIE_AGE,
+    });
 
     return {
-      data: accessToken,
+      accessToken: tokens.accessToken,
     };
+  }
+
+  @Get('refresh')
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refreshToken = req.cookies['refreshToken'];
+
+    if (!refreshToken) {
+      throw new UnauthorizedException('Invalid refresh token');
+    }
+
+    const payload = await this.authService.verifyRefreshToken(refreshToken);
+
+    const accessToken = await this.authService.generateToken('ACCESS', payload);
+    const newRefreshToken = await this.authService.generateToken(
+      'REFRESH',
+      payload,
+    );
+
+    res.cookie('refreshToken', newRefreshToken, {
+      httpOnly: true,
+      secure: this.configService.get('NODE_ENV') === 'production',
+      maxAge: REFRESH_TOKEN_COOKIE_AGE,
+    });
+
+    return { accessToken };
   }
 }
