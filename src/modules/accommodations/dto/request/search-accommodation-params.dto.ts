ce2d8732a45e68930979535426bd5afa71ba2add
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-11-14 21:53:01 +0500

    fix: optional keyword added

--- src/modules/accommodations/dto/request/search-accommodation-params.dto.ts
+++ src/modules/accommodations/dto/request/search-accommodation-params.dto.ts
@@ -4,32 +4,32 @@ import { TransformToDate, TransformToInt } from 'src/shared/decorators';
 export class SearchAccommodationQueryParamsDto {
   @IsString()
   @IsOptional()
-  search: string;
+  search?: string;
 
   @IsString()
   @IsOptional()
-  category: string;
+  category?: string;
 
   @TransformToDate()
   @IsDate()
   @IsOptional()
-  fromDate: Date;
+  fromDate?: Date;
 
   @TransformToDate()
   @IsDate()
   @IsOptional()
-  toDate: Date;
+  toDate?: Date;
 
   @TransformToInt()
   @IsInt()
   @IsOptional()
   @Min(1)
-  page: number = 1;
+  page?: number = 1;
 
   @TransformToInt()
   @IsInt()
   @IsOptional()
   @Min(1)
   @Max(100)
-  limit: number = 10;
+  limit?: number = 10;
 }
