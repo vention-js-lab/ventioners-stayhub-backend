b33923167d12ed6bf0cdd3700b5103ef19d408fa
tiescl <anvar.madvaliev@icloud.com>
2024-11-21 17:09:01 +0500

    feat: define service methods for interacting with min.io instance

---
+++ src/modules/minio/minio.service.ts
@@ -0,0 +1,80 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as Minio from 'minio';
+
+@Injectable()
+export class MinioService {
+  private minioClient: Minio.Client;
+  private bucketName: string;
+
+  constructor(private readonly configService: ConfigService) {
+    this.minioClient = new Minio.Client({
+      endPoint: this.configService.get('MINIO_HOST'),
+      port: Number(this.configService.get('MINIO_PORT')),
+      useSSL: false,
+      accessKey: this.configService.get('MINIO_ROOT_USER'),
+      secretKey: this.configService.get('MINIO_ROOT_PASSWORD'),
+    });
+
+    this.bucketName = this.configService.get('MINIO_BUCKET_NAME');
+    this.createBucketIfNotExists();
+  }
+
+  async createBucketIfNotExists() {
+    const bucketExists = await this.minioClient.bucketExists(this.bucketName);
+    if (!bucketExists) {
+      await this.minioClient.makeBucket(this.bucketName);
+
+      const bucketPolicyRead = {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Principal: '*',
+            Action: ['s3:GetObject'],
+            Resource: [`arn:aws:s3:::${this.bucketName}/*`],
+          },
+        ],
+      };
+
+      await this.minioClient.setBucketPolicy(
+        this.bucketName,
+        JSON.stringify(bucketPolicyRead),
+      );
+    }
+  }
+
+  async uploadFile(file: Express.Multer.File) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+
+    try {
+      await this.minioClient.putObject(
+        this.bucketName,
+        fileName,
+        file.buffer,
+        file.size,
+      );
+
+      return fileName;
+    } catch (err) {
+      throw new InternalServerErrorException('Object upload failed');
+    }
+  }
+
+  async deleteFile(fileName: string) {
+    try {
+      await this.minioClient.removeObject(this.bucketName, fileName);
+    } catch (err) {
+      console.log(err);
+      throw new InternalServerErrorException('Object removal failed');
+    }
+  }
+
+  async deleteFiles(fileNames: string[]) {
+    try {
+      await this.minioClient.removeObjects(this.bucketName, fileNames);
+    } catch (err) {
+      throw new InternalServerErrorException('Objects removal failed');
+    }
+  }
+}
