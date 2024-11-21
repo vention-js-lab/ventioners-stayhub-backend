5cc13a6b07cd2d8b9a35590150ae47d7f454e5e4
tiescl <anvar.madvaliev@icloud.com>
2024-11-21 17:09:01 +0500

    feat: define service methods for interacting with min.io instance

--- src/modules/minio/minio.service.ts
+++ src/modules/minio/minio.service.ts
@@ -1,96 +1,55 @@
-import {
-  Injectable,
-  InternalServerErrorException,
-  Logger,
-} from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { AccessPolicy, BucketName } from './minio.constants';
-import { isProd } from 'src/shared/helpers';
-import { randomUUID } from 'crypto';
 import * as Minio from 'minio';
 
 @Injectable()
 export class MinioService {
   private minioClient: Minio.Client;
-  private readonly logger = new Logger(MinioService.name, { timestamp: true });
+  private bucketName: string;
 
   constructor(private readonly configService: ConfigService) {
     this.minioClient = new Minio.Client({
       endPoint: this.configService.get('MINIO_HOST'),
       port: Number(this.configService.get('MINIO_PORT')),
-      useSSL: isProd(this.configService.get('NODE_ENV')),
+      useSSL: false,
       accessKey: this.configService.get('MINIO_ROOT_USER'),
       secretKey: this.configService.get('MINIO_ROOT_PASSWORD'),
     });
+
+    this.bucketName = this.configService.get('MINIO_BUCKET_NAME');
+    this.createBucketIfNotExists();
   }
 
-  async initializeBucket(
-    bucketName = BucketName.Images,
-    accessPolicy = AccessPolicy.Private,
-  ) {
-    await this.createBucketIfNotExists(bucketName);
-
-    if (accessPolicy == AccessPolicy.Public) {
-      await this.makeBucketPublic(bucketName);
-    }
-  }
-
-  private async makeBucketPublic(bucketName: BucketName) {
-    const bucketPolicyRead = {
-      Version: '2012-10-17',
-      Statement: [
-        {
-          Effect: 'Allow',
-          Principal: '*',
-          Action: ['s3:GetObject'],
-          Resource: [`arn:aws:s3:::${bucketName}/*`],
-        },
-      ],
-    };
-
-    let prevPolicy = bucketPolicyRead;
-    try {
-      const policy = await this.minioClient.getBucketPolicy(bucketName);
-      prevPolicy = JSON.parse(policy);
-    } catch (err) {
-      this.logger.error('Could not fetch the bucket policy', err.stack);
-    }
-
-    if (
-      !prevPolicy['Statement'].some(
-        (stat: unknown) =>
-          stat['Action'].includes('s3:GetObject') &&
-          stat['Resource'].includes(`arn:aws:s3:::${bucketName}/*`),
-      )
-    ) {
-      prevPolicy['Statement'].push(bucketPolicyRead.Statement[0]);
-    }
-
-    try {
-      await this.minioClient.setBucketPolicy(
-        bucketName,
-        JSON.stringify(prevPolicy),
-      );
-    } catch (err) {
-      this.logger.error('Could not set the bucket policy', err.stack);
-      throw new InternalServerErrorException('Failed to initialize bucket');
-    }
-  }
-
-  private async createBucketIfNotExists(bucketName: BucketName) {
-    const bucketExists = await this.minioClient.bucketExists(bucketName);
-
+  async createBucketIfNotExists() {
+    const bucketExists = await this.minioClient.bucketExists(this.bucketName);
     if (!bucketExists) {
-      await this.minioClient.makeBucket(bucketName);
+      await this.minioClient.makeBucket(this.bucketName);
+
+      const bucketPolicyRead = {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Principal: '*',
+            Action: ['s3:GetObject'],
+            Resource: [`arn:aws:s3:::${this.bucketName}/*`],
+          },
+        ],
+      };
+
+      await this.minioClient.setBucketPolicy(
+        this.bucketName,
+        JSON.stringify(bucketPolicyRead),
+      );
     }
   }
 
-  async uploadFile(file: Express.Multer.File, bucketName = BucketName.Images) {
-    const fileName = `${randomUUID()}-${file.originalname}`;
+  async uploadFile(file: Express.Multer.File) {
+    const fileName = `${Date.now()}-${file.originalname}`;
 
     try {
       await this.minioClient.putObject(
-        bucketName,
+        this.bucketName,
         fileName,
         file.buffer,
         file.size,
@@ -98,25 +57,23 @@ export class MinioService {
 
       return fileName;
     } catch (err) {
-      this.logger.error('Could not upload the object', err.stack);
       throw new InternalServerErrorException('Object upload failed');
     }
   }
 
-  async deleteFile(fileName: string, bucketName = BucketName.Images) {
+  async deleteFile(fileName: string) {
     try {
-      await this.minioClient.removeObject(bucketName, fileName);
+      await this.minioClient.removeObject(this.bucketName, fileName);
     } catch (err) {
-      this.logger.error('Could not delete the object', err.stack);
+      console.log(err);
       throw new InternalServerErrorException('Object removal failed');
     }
   }
 
-  async deleteFiles(fileNames: string[], bucketName = BucketName.Images) {
+  async deleteFiles(fileNames: string[]) {
     try {
-      await this.minioClient.removeObjects(bucketName, fileNames);
+      await this.minioClient.removeObjects(this.bucketName, fileNames);
     } catch (err) {
-      this.logger.error('Could not delete the objects', err.stack);
       throw new InternalServerErrorException('Objects removal failed');
     }
   }
