1b1f26ebe3e5703d444d8a62f2fe984b6548a6df
Anvar Madvaliev <112773086+tiescl@users.noreply.github.com>
2024-11-26 22:52:29 +0500

    fix: return user instead of jwt payload when validating refresh tokens (#28)

--- src/modules/auth/guards/refresh-token.guard.ts
+++ src/modules/auth/guards/refresh-token.guard.ts
@@ -5,11 +5,11 @@ import { AuthConfig } from 'src/shared/configs';
 @Injectable()
 export class RefreshTokenGuard extends AuthGuard(AuthConfig.RefreshTokenKey) {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  handleRequest(err: any, payload: any, _info: any) {
-    if (err || !payload) {
+  handleRequest(err: any, user: any, _info: any) {
+    if (err || !user) {
       throw new UnauthorizedException('Invalid refresh token');
     }
 
-    return payload;
+    return user;
   }
 }
