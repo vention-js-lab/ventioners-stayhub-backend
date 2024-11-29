bdca342ffcc8426e489f985279c3afa4f40336dd
purple-mountain <lmwrong92@gmail.com>
2024-11-29 11:36:20 +0000

    chore: added index.ts exports

--- src/modules/bookings/dto/request/create-booking.dto.ts
+++ src/modules/bookings/dto/request/create-booking.dto.ts
@@ -1,9 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsDate, IsInt, IsUUID, Min } from 'class-validator';
-import { TransformToDate } from 'src/shared/transformers';
-import { TransformToYearMonthDay } from 'src/shared/transformers/transform-to-year-month-day.transformer';
-import { IsAfter } from 'src/shared/validators/is-date-after.validator';
-import { IsFutureDate } from 'src/shared/validators/is-future-date.validator';
+import {
+  TransformToDate,
+  TransformToYearMonthDay,
+} from 'src/shared/transformers';
+import { IsAfter, IsFutureDate } from 'src/shared/validators';
 
 export class CreateBookingReqDto {
   @IsUUID('4')
