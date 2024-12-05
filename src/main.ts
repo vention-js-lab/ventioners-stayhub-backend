bd63cb00c72199788aff5cefe36c04e69d64c6a2
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-05 12:40:44 +0500

    feat: rawBody enabled for stripe webhook

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 import { swaggerConfig } from './shared/configs/swagger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
 
   const configService = app.get(ConfigService<EnvConfig>);
 
