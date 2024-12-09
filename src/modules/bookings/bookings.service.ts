5bc6f632eae4e55bbc46d4da51c1d17859bb1a43
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-09 11:37:13 +0500

    fix: user included in bookingById

--- src/modules/bookings/bookings.service.ts
+++ src/modules/bookings/bookings.service.ts
@@ -150,6 +150,7 @@ export class BookingsService {
   async getBookingById(bookingId: string): Promise<Booking> {
     const booking = await this.bookingRepository.findOne({
       where: { id: bookingId },
+      relations: ['user'],
     });
     if (!booking) {
       throw new NotFoundException(`Booking with id ${bookingId} not found`);
