47d54cca06242fed5cc715d85f072cb4ee7caf78
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2024-12-11 09:54:13 +0500

    fix: unused middleware removed

--- src/modules/stripe/stripe.module.ts
+++ src/modules/stripe/stripe.module.ts
@@ -1,15 +1,9 @@
-import {
-  forwardRef,
-  MiddlewareConsumer,
-  Module,
-  NestModule,
-} from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { ConfigModule } from '@nestjs/config';
 import { StripeController } from './stripe.controller';
 import { BookingsModule } from '../bookings/bookings.module';
 import { AccommodationsModule } from '../accommodations/accommodations.module';
-import { RawBodyMiddleware } from '../../shared/middlewares';
 import { PaymentsModule } from '../payments/payments.module';
 
 @Module({
@@ -23,8 +17,4 @@ import { PaymentsModule } from '../payments/payments.module';
   providers: [StripeService],
   exports: [StripeService],
 })
-export class StripeModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(RawBodyMiddleware).forRoutes('stripe/webhook');
-  }
-}
+export class StripeModule {}
