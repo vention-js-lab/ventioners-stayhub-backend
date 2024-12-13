c3dcab2601a77d8cb2b257f94a3eb7e977723908
Bek Shoyatbek <92543629+bek-shoyatbek@users.noreply.github.com>
2024-12-13 20:25:40 +0500

    test: logs added to stripe service (#57)

--- src/modules/stripe/stripe.service.ts
+++ src/modules/stripe/stripe.service.ts
@@ -180,6 +180,8 @@ export class StripeService {
 
   private verifyEvent(body: string | Buffer, signature: string) {
     try {
+      this.logger.log('Verifying Stripe webhook signature');
+      this.logger.log(`Webhook secret: ${this.webhookSecret}`);
       return this.stripe.webhooks.constructEvent(
         body,
         signature,
