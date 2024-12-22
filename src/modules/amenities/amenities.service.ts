2e875ee6fdd597d59001526bbead0a63fe1550b6
purple-mountain <88972232+purple-mountain@users.noreply.github.com>
2024-12-22 20:54:11 +0500

    [FEAT-97]: Added caching for amenities and categories (#79)

--- src/modules/amenities/amenities.service.ts
+++ src/modules/amenities/amenities.service.ts
@@ -1,24 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Amenity } from './entities';
 import { In, Repository } from 'typeorm';
+import { RedisService } from 'src/redis/redis.service';
+import { parseJSON } from 'src/shared/helpers';
 
 @Injectable()
 export class AmenitiesService {
   constructor(
     @InjectRepository(Amenity)
     private readonly amenityRepository: Repository<Amenity>,
+    private readonly redisService: RedisService,
   ) {}
 
+  private readonly logger = new Logger('AmenitiesService');
+
   async getAllAmenities(acceptLanguage: string = 'en'): Promise<Amenity[]> {
+    const cacheKey = `amenities:${acceptLanguage}`;
+    const cachedAmenities = await this.redisService.get(cacheKey);
+
+    if (cachedAmenities) {
+      const amenities = parseJSON<Amenity[]>(cachedAmenities);
+
+      if (Array.isArray(amenities)) {
+        this.logger.log(`Cache hit: ${cacheKey}`);
+
+        return amenities;
+      }
+    }
+
     const amenities = await this.amenityRepository.find();
 
-    return amenities.map((amenity) => ({
+    const localizedAmenities = amenities.map((amenity) => ({
       ...amenity,
       name: acceptLanguage === 'ru' ? amenity.name_ru : amenity.name,
       description:
         acceptLanguage === 'ru' ? amenity.description_ru : amenity.description,
     }));
+
+    await this.redisService.set(
+      cacheKey,
+      JSON.stringify(localizedAmenities),
+      5 * 24 * 60 * 60,
+    );
+
+    return localizedAmenities;
   }
 
   async getAmenitiesByIds(ids: string[]): Promise<Amenity[]> {
